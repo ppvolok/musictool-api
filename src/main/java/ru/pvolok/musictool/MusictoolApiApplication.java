2c0aeff093a6eede7e526c09751a21185eba38af
Pavel Volokitin <pvolok@gmail.com>
2020-01-14 09:55:41 +0300

    Initial commit

---
+++ src/main/java/ru/pvolok/musictool/MusictoolApiApplication.java
@@ -0,0 +1,13 @@
+package ru.pvolok.musictool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusictoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusictoolApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pvolok/musictool/MusictoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pvolok.musictool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusictoolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
